f882038ada9b9376da60e5162976db08dc067e4d
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-03-10 11:33:49 +0200

    format switch themes file

--- calculator/src/switchTheme.js
+++ calculator/src/switchTheme.js
@@ -1,22 +1,22 @@
-const switchButton = document.getElementById("reverse_theme");
+const switchButton = document.getElementById('reverse_theme');
 const switchNameTitle = document.getElementById('theme_title');
 switchButton.addEventListener('click', switchThemeColor);
 
 const cssFileName = document.querySelectorAll('link');
 
 const themeNames = {
-    white: 'Switch to white theme',
-    dark: 'Switch to dark theme'
+  white: 'Switch to white theme',
+  dark: 'Switch to dark theme',
 };
 
 function switchThemeColor(event) {
-    const hasChecked = event.target.checked;
-    if (hasChecked) {
-        cssFileName[1].href = 'http://127.0.0.1:5500/public/css/white/white.css';
-        switchNameTitle.textContent = themeNames.dark;
-        return
-    }
+  const hasChecked = event.target.checked;
+  if (hasChecked) {
+    cssFileName[1].href = 'http://127.0.0.1:5500/public/css/white/white.css';
+    switchNameTitle.textContent = themeNames.dark;
+    return;
+  }
 
-    cssFileName[1].href = 'http://127.0.0.1:5500/public/css/dark/dark.css';
-    switchNameTitle.textContent = themeNames.white;
-};
+  cssFileName[1].href = 'http://127.0.0.1:5500/public/css/dark/dark.css';
+  switchNameTitle.textContent = themeNames.white;
+}
