2bc81ab724dd8c48d8811ecd3634ae4c146cd73e
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-03-10 11:34:25 +0200

    add mathOperation file and extract simple math functions there

---
+++ calculator/src/helpers/mathOperations.js
@@ -0,0 +1,15 @@
+export function sum(sum1, sum2) {
+  return sum1 + sum2
+};
+
+export function subtract(sum1, sum2) {
+  return sum1 - sum2
+};
+
+export function multiply(sum1, sum2) {
+  return sum1 * sum2
+};
+
+export function divide(sum1, sum2) {
+  return sum1 / sum2
+};
