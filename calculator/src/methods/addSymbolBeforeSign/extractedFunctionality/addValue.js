06152d89795d169966207b7d73da797bb8e15eb4
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-03-16 20:20:36 +0200

    change some logic in addValue function to can user directly use . sign and this add zero before decimal sign

--- calculator/src/methods/addSymbolBeforeSign/extractedFunctionality/addValue.js
+++ calculator/src/methods/addSymbolBeforeSign/extractedFunctionality/addValue.js
@@ -1,12 +1,12 @@
 import { numbers, signs } from '../../../config.js';
 
 export const addValue = ({ firstDigit, value, hasDot }) => {
-  const hasValue = firstDigit?.length > 0;
+  const hasValue = firstDigit > 0;
   const isValueNull = value === numbers[0];
   const isValueDot = value === signs['.'];
 
   if (isValueNull && !hasValue) {
-    return (firstDigit = '');
+    return (firstDigit = value);
   }
 
   if (isValueNull && hasValue) {
