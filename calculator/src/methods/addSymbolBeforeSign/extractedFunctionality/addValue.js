7f3be511d53736bf74dedd8341d7f661ab81bab4
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-03-24 12:53:58 +0200

    update add values function to update and take all values directly from config js predefined and saved values

--- calculator/src/methods/addSymbolBeforeSign/extractedFunctionality/addValue.js
+++ calculator/src/methods/addSymbolBeforeSign/extractedFunctionality/addValue.js
@@ -1,19 +1,18 @@
-import { numbers, signs } from '../../../config.js';
+import { ZERO_DOT, numbers, signs, valuesObject } from '../../../config.js';
 
-export const addValue = ({ firstDigit, value, hasDot }) => {
-  const hasValue = firstDigit > 0;
+export const addValue = ({ value, hasDot }) => {
+  const hasLength = !!valuesObject.firstDigit?.length > 0;
+  // const hasValue = !!valuesObject.firstDigit > 0;
   const isValueNull = value === numbers[0];
   const isValueDot = value === signs['.'];
 
-  if (isValueNull && !hasValue) {
-    return (firstDigit = value);
+  if (!hasDot && !hasLength && isValueDot) {
+    valuesObject.firstDigit = ZERO_DOT;
+    return;
+  } else if (!hasLength && isValueNull) {
+    valuesObject.firstDigit = value;
+    return;
+  } else {
+    valuesObject.firstDigit += value;
   }
-
-  if (isValueNull && hasValue) {
-    return (firstDigit += value);
-  } else if (isValueDot && hasValue && !hasDot) {
-    return (firstDigit += value);
-  }
-
-  return (firstDigit += value);
 };
