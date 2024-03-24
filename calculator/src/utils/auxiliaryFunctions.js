d63c07bc243d2100241018ae28512067218b7f89
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-03-24 13:00:42 +0200

    update some of the functions from auxiliary function

--- calculator/src/utils/auxiliaryFunctions.js
+++ calculator/src/utils/auxiliaryFunctions.js
@@ -1,124 +1,132 @@
-import { signs } from '../config.js';
+import {
+  signs,
+  textAreaSelect,
+  textAreaValue,
+  THE_SUM_SYMBOLS,
+  valuesObject,
+} from '../config.js';
 import * as actions from './actions.js';
 
 export const action = {
-  x: actions.multiply,
-  '÷': actions.divide,
-  '+': actions.sum,
-  '-': actions.subtract,
-  '√': actions.squareRoot,
+  [signs.x]: actions.multiply,
+  [signs['÷']]: actions.divide,
+  [signs['+']]: actions.sum,
+  [signs['-']]: actions.subtract,
+  [signs['√']]: actions.squareRoot,
+  [signs['±']]: actions.reverse,
   '1%': actions.firstDigitPercent,
   '2%': actions.secondDigitPercent,
-  '±': actions.reverse,
 };
 
 export function isInt(value) {
-  // const regexp = /(^\d+.$)||(^\.\d+$)||(^\d+$)||(^\d+\.\d+$)/g;
-
-  const hasValueIncludesPercent = checkForSymbol(value, [signs['%']]);
   const hasValueIncludesSquare = checkForSymbol(value, [signs['√']]);
+  const hasValueIncludesPercent = checkForSymbol(value, [signs['%']]);
 
-  if (value) {
-    if (hasValueIncludesPercent || hasValueIncludesSquare) {
-      if (value.toString().includes('√')) {
-        value = value.split('');
-        value.shift();
-        value = value.join('');
-      }
-      if (value.toString().includes('%')) {
-        value = value.split('');
-        value.pop();
-        value = value.join('');
-      }
-    }
+  if (hasValueIncludesPercent || hasValueIncludesSquare) {
+    value = value.split('');
+    if (hasValueIncludesSquare) value.shift();
+    if (hasValueIncludesPercent) value.pop();
+    value = value.join('');
   }
 
-  if (!value || !value.toString().includes('.')) {
+  const hasValueIncludesPeriod = value && checkForSymbol(value, [signs['.']]);
+  if (!hasValueIncludesPeriod) {
     return true;
-  } else {
-    return false;
   }
 }
 
-export function calculateSum(arr) {
-  let sum = 0;
-  for (let i = 0; i <= arr.length - 1; i += 2) {
-    arr[i] = arr[i].toString();
-    if (arr[i].includes('%') && arr[i].includes('√')) {
-      arr[i] = arr[i].slice(0, arr[i].length - 1);
-      if (i > 0) {
-        let square = action['√'](arr[i]);
-        let percent = action['1%'](Number(square));
-        sum += percent;
-      } else {
-        sum += Number(action['√'](arr[i]));
-        sum = action['1%'](Number(sum));
-      }
-      arr[i] = arr[i].split('');
-      arr[i].push('%');
-      arr[i] = arr[i].join('');
-    } else if (arr[i].includes('%') || arr[i].includes('√')) {
-      if (arr[i].includes('%')) {
-        if (i == 0) {
-          arr[i] = arr[i].slice(0, arr[i].length - 1);
-          sum += action['1%'](Number(arr[i]));
-          arr[i] = arr[i].split('');
-          arr[i].push('%');
-          arr[i] = arr[i].join('');
-        } else {
-          arr[i] = arr[i].slice(0, arr[i].length - 1);
-          sum = sumTheSum(
-            Number(sum),
-            arr[i - 1],
-            action['2%'](Number(sum), Number(arr[i]))
-          );
-          arr[i] = arr[i].split('');
-          arr[i].push('%');
-          arr[i] = arr[i].join('');
-        }
-      } else if (arr[i].includes('√')) {
-        sum = sumTheSum(sum, arr[i - 1], Number(action['√'](arr[i])));
-      }
-    } else {
-      sum = sumTheSum(sum, arr[i - 1], arr[i]);
-    }
+function calculateTheSum(sum, symbol, currentDigit) {
+  const sumNumber = Number(sum);
+  const currentDigitNumber = Number(currentDigit);
+  if (THE_SUM_SYMBOLS[symbol]) {
+    return (sum = action[symbol](sumNumber, currentDigitNumber));
+  } else if (!symbol) {
+    return (sum += Number(currentDigit));
+  }
+}
 
-    function sumTheSum(sum, symbol, currentDigit) {
-      if (symbol == '+') {
-        return (sum = action['+'](Number(sum), Number(currentDigit)));
-      } else if (symbol == '-') {
-        return (sum = action['-'](Number(sum), Number(currentDigit)));
-      } else if (symbol == 'x') {
-        return (sum = action['x'](Number(sum), Number(currentDigit)));
-      } else if (symbol == '÷') {
-        return (sum = action['÷'](Number(sum), Number(currentDigit)));
-      } else if (!symbol) {
-        return (sum += Number(currentDigit));
-      }
+const calculateSumWithPercentAndSquare = (value, index, sum) => {
+  value = value.toString();
+  value = value.slice(0, value.length - 1);
+
+  if (index > 0) {
+    const square = action['√'](value);
+    const percent = action['1%'](Number(square));
+    sum += percent;
+  } else {
+    sum += Number(action['√'](value));
+    sum = action['1%'](Number(sum));
+  }
+  value = value.split('');
+  value.push('%');
+  value = value.join('');
+
+  return sum;
+};
+
+const calculateSumWithPercentOrSquare = (values, index, sum) => {
+  const hasValueIncludesPercent = checkForSymbol(values[index], [signs['%']]);
+  const hasValueIncludesSquare = checkForSymbol(values[index], [signs['√']]);
+
+  if (hasValueIncludesPercent) {
+    if (index > 0) {
+      values[index] = values[index].slice(0, values[index].length - 1);
+      sum = calculateTheSum(
+        values[index - 1],
+        action['2%'](Number(sum), Number(values[index]))
+      );
+      values[index] = values[index].split('');
+      values[index].push('%');
+      values[index] = values[index].join('');
+      return;
+    }
+    values[index] = values[index].slice(0, values[index].length - 1);
+    sum += action['1%'](Number(values[index]));
+    values[index] = values[index].split('');
+    values[index].push('%');
+    values[index] = values[index].join('');
+  } else if (hasValueIncludesSquare) {
+    sum = calculateTheSum(
+      sum,
+      values[index - 1],
+      Number(action['√'](values[index]))
+    );
+  }
+
+  return sum;
+};
+
+export function calculateSum(array) {
+  let sum = 0;
+  for (let i = 0; i <= array.length - 1; i += 2) {
+    const value = array[i].toString();
+    const hasValueIncludesPercent = checkForSymbol(value, [signs['%']]);
+    const hasValueIncludesSquare = checkForSymbol(value, [signs['√']]);
+    if (hasValueIncludesPercent && hasValueIncludesSquare) {
+      sum = calculateSumWithPercentAndSquare(array[i], i, sum);
+    } else if (hasValueIncludesPercent || hasValueIncludesSquare) {
+      sum = calculateSumWithPercentOrSquare(array, i, sum);
+    } else {
+      sum = calculateTheSum(sum, array[i - 1], array[i]);
     }
   }
+
   return sum;
 }
 
-export function printResult(array, truthy, sum, selectedElement) {
-  if ((array.length > 2 && truthy) || (array.length < 2 && truthy)) {
-    selectedElement.value = `${array.join('')} \r\n${sum}`;
-  } else {
-    selectedElement.value = `${array.join('')}`;
-  }
-}
+export const printResult = () =>
+  (textAreaSelect.value = `${textAreaValue.join('')} \r\n${valuesObject.sum}`);
 
 export const checkForSymbol = (data, symbols) => {
-  if (
-    Array.isArray(symbols) &&
-    symbols?.length > 0 &&
-    typeof data === 'string'
-  ) {
+  const isDataAvailable = !!data;
+  const stringData = data?.toString();
+  const isDataString = isDataAvailable ? typeof stringData === 'string' : false;
+  if (Array.isArray(symbols) && symbols?.length > 0 && isDataString) {
     for (const symbol of symbols) {
-      const hasSymbolIncluded = data?.includes(symbol);
-      if (hasSymbolIncluded) return true;
+      const hasSymbolIncluded = stringData?.includes(symbol);
+      if (!hasSymbolIncluded) return false;
     }
-    return false;
+    return true;
   }
 };
 
