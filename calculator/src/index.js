b55127edc13e9d7e83515a49be7466d1aeda8709
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-03-24 12:46:37 +0200

    extract most of the logic for calculation in separate files and reused it in index.js file

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -1,4 +1,9 @@
-import { signs, calculationSigns, calculatorValues } from './config.js';
+import {
+  signs,
+  calculationSigns,
+  CALCULATOR_VALUES,
+  valuesObject,
+} from './config.js';
 import { calculateAfterSign } from './methods/addSymbolAfterSign/calculateAfterSign.js';
 import { calculateBeforeSign } from './methods/addSymbolBeforeSign/calculateBeforeSign.js';
 import { deleteFunctionality } from './methods/deleteSymbolsToFirstSign/deleteSymbolsToFirstSign.js';
@@ -16,101 +21,32 @@ Array.from(buttonBoxSelects).forEach((button) =>
   button.addEventListener('click', calculate)
 );
 
-let firstDigit = '';
-let secondDigit = '';
-let sum = 0;
-let sign = '';
-const textAreaValue = [];
-
 function calculate(event) {
   event.preventDefault();
-  const value = event.target.getAttribute('value');
-  const isSignCouldAdd = !!(calculationSigns[value] && firstDigit);
+  valuesObject.eventValue = event.target.getAttribute('value');
+  const isSignCouldAdd =
+    valuesObject.firstDigit && !!calculationSigns[valuesObject.eventValue];
 
-  if (calculatorValues[value]) {
+  if (CALCULATOR_VALUES[valuesObject.eventValue]) {
     // it's a part of code who handle values after signs.
-    if (sign) {
-      const symbolsAfterSign = calculateAfterSign({
-        value,
-        secondDigit,
-        sign,
-        textAreaValue,
-      });
-      secondDigit = symbolsAfterSign.secondDigit;
-      sign = symbolsAfterSign.sign;
+    if (valuesObject.sign) {
+      calculateAfterSign();
     } else {
       // it's a part of code who handle values before signs.
-      const symbolsBeforeSign = calculateBeforeSign({
-        value,
-        firstDigit,
-        sign,
-        textAreaValue,
-        sum,
-      });
-      firstDigit = symbolsBeforeSign?.firstDigit;
-      sign = symbolsBeforeSign?.sign;
+      calculateBeforeSign();
     }
   } else if (isSignCouldAdd) {
     // it's a part of code who respond for math signs.
-    const addMinusSign = addSign({
-      sign,
-      textAreaValue,
-      value,
-      firstDigit,
-      secondDigit,
-      currentSign: signs[value],
-      sum,
-    });
-    sign = addMinusSign?.sign;
-    firstDigit = addMinusSign?.firstDigit;
-    secondDigit = addMinusSign?.secondDigit;
-  } else if (value === 'DEL') {
-    const deleteLeftSymbols = deleteFunctionality({
-      textAreaValue,
-      sign,
-      sum,
-      secondDigit,
-      firstDigit,
-    });
-    sign = deleteLeftSymbols?.sign;
-    sum = deleteLeftSymbols?.sum;
-    secondDigit = deleteLeftSymbols?.secondDigit;
-    firstDigit = deleteLeftSymbols?.firstDigit;
-  } else if (value === '=') {
-    const equalResults = addEqualSign({
-      sum,
-      firstDigit,
-      secondDigit,
-      sign,
-      textAreaValue,
-    });
-    sum = equalResults?.sum;
-    firstDigit = equalResults?.firstDigit;
-    secondDigit = equalResults?.secondDigit;
-    sign = equalResults?.sign;
-  } else if (value === '%') {
-    const percent = addPercent({
-      sign,
-      textAreaValue,
-      firstDigit,
-      secondDigit,
-      sum,
-      value,
-    });
-    sum = percent.sum;
-    firstDigit = percent.firstDigit;
-    secondDigit = percent.secondDigit;
-  } else if (value === '.') {
-    const period = addPeriod({
-      firstDigit,
-      secondDigit,
-      textAreaValue,
-      value,
-      sign,
-    });
-    firstDigit = period.firstDigit;
-    secondDigit = period.secondDigit;
+    addSign(signs[valuesObject.eventValue]);
+  } else if (valuesObject.eventValue === 'DEL') {
+    deleteFunctionality();
+  } else if (valuesObject.eventValue === '=') {
+    addEqualSign();
+  } else if (valuesObject.eventValue === '%') {
+    addPercent();
+  } else if (valuesObject.eventValue === '.') {
+    addPeriod();
   }
 
-  sumUpdate({ textAreaValue, isSecondDigit: true, sum });
+  sumUpdate();
 }
