fb9ceaa4589963a0aa8a452809b37274c0c19376
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-03-24 12:58:06 +0200

    update "clear every next value of calculator" to work directly with predefined values form config.js

--- calculator/src/methods/deleteSymbolsToFirstSign/extractedFunctionality/clearEveryNextValueOfCalculator.js
+++ calculator/src/methods/deleteSymbolsToFirstSign/extractedFunctionality/clearEveryNextValueOfCalculator.js
@@ -1,78 +1,46 @@
-import { textAreaSelect, signs } from '../../../config.js';
+import {
+  textAreaSelect,
+  signs,
+  valuesObject,
+  textAreaValue,
+} from '../../../config.js';
 import {
   calculateSum,
   checkForSymbol,
   checkIsNumber,
   printResult,
 } from '../../../utils/auxiliaryFunctions.js';
+
 const symbolsToCheck = ['0.', '0', signs['√'], signs['%']];
 
-const reduceSymbolsAndNumbers = ({
-  sign,
-  textAreaValue,
-  value,
-  firstDigit,
-  secondDigit,
-  sum,
-}) => {
+const reduceSymbolsAndNumbers = (value) => {
   const singIndex = textAreaValue?.length - 2;
   const isValueLongLength = value?.length > 1;
 
   if (isValueLongLength) {
     textAreaValue[textAreaValue.length - 1] = value.slice(0, -1);
-    secondDigit = value;
+    valuesObject.secondDigit = value;
   } else {
     textAreaValue.pop();
-    firstDigit = sum;
-    secondDigit = '';
-    printResult(textAreaValue, true, sum, textAreaSelect);
+    valuesObject.firstDigit = valuesObject.sum;
+    valuesObject.secondDigit = '';
+    printResult();
   }
-  sign = textAreaValue[singIndex];
-  sum = calculateSum(textAreaValue);
-  textAreaSelect.value = `${textAreaValue.join('')} \r\n${sum}`;
-
-  return {
-    sign,
-    sum,
-    firstDigit,
-    secondDigit,
-  };
+  valuesObject.sign = textAreaValue[singIndex];
+  valuesObject.sum = calculateSum(textAreaValue);
+  textAreaSelect.value = `${textAreaValue.join('')} \r\n${valuesObject.sum}`;
 };
 
-export const clearEveryNextValueOfCalculator = ({
-  sign,
-  textAreaValue,
-  secondDigit,
-  sum,
-  firstDigit,
-}) => {
+export const clearEveryNextValueOfCalculator = () => {
   const value = textAreaValue[textAreaValue?.length - 1];
   const isValueNumber = checkIsNumber(value);
   const hasSymbolIncluded = checkForSymbol(value, symbolsToCheck);
 
   if (isValueNumber || hasSymbolIncluded) {
-    const symbolsAndNumbersReducer = reduceSymbolsAndNumbers({
-      sum,
-      sign,
-      textAreaValue,
-      value,
-      firstDigit,
-      secondDigit,
-    });
-    firstDigit = symbolsAndNumbersReducer.firstDigit;
-    secondDigit = symbolsAndNumbersReducer.secondDigit;
-    sign = symbolsAndNumbersReducer.sign;
-    sum = symbolsAndNumbersReducer.sum;
+    reduceSymbolsAndNumbers(value);
   } else {
     textAreaValue.pop();
-    sign = '';
-    printResult(textAreaValue, true, sum, textAreaSelect);
+    valuesObject.sign = '';
+    printResult();
   }
-
-  return {
-    sign,
-    secondDigit,
-    sum,
-    firstDigit,
-  };
 };
