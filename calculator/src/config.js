c4856c319aca24fb6ef7c24c9e88265149361d2f
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-03-31 15:03:49 +0300

    add some changes to config file to take values and keys directly from signs constant

--- calculator/src/config.js
+++ calculator/src/config.js
@@ -4,12 +4,14 @@ export const textAreaSelect = document.querySelector(
 export const valuesObject = {
   firstDigit: '',
   secondDigit: '',
-  sum: '',
+  sum: 0,
   sign: '',
   eventValue: '',
+  errors: '',
 };
 export const textAreaValue = [];
 
+export const DIGIT_REGEX = /-?\d+(\.\d+)?/g;
 export const ZERO_DOT = '0.';
 export const numbers = {
   1: '1',
@@ -34,26 +36,26 @@ export const signs = {
   '.': '.',
 };
 export const reversedSigns = {
-  '-': signs['+'],
-  '+': signs['-'],
+  [signs['-']]: signs['+'],
+  [signs['+']]: signs['-'],
 };
 export const calculationSigns = {
-  '+': signs['+'],
-  '-': signs['-'],
-  x: signs.x,
-  '÷': signs['÷'],
+  [signs['+']]: signs['+'],
+  [signs['-']]: signs['-'],
+  [signs['x']]: signs['x'],
+  [signs['÷']]: signs['÷'],
 };
 export const CALCULATOR_VALUES = {
   ...numbers,
-  '±': signs['±'],
-  '√': signs['√'],
+  [signs['±']]: signs['±'],
+  [signs['√']]: signs['√'],
 };
 
-export const BUTTON_EVENTS = {
+export const BUTTONS_EVENTS = {
   DEL: 'DEL',
-  '%': '%',
+  [signs['%']]: signs['%'],
   '=': '=',
-  '.': '.',
+  [signs['.']]: signs['.'],
 };
 
 export const THE_SUM_SYMBOLS = {
