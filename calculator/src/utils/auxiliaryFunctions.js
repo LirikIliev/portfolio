f82875fbcb4b7be5a992111a98b0725361d1a08c
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-03-31 15:15:25 +0300

    change some of functions to equal of all changes of the app into the auxiliary function

--- calculator/src/utils/auxiliaryFunctions.js
+++ calculator/src/utils/auxiliaryFunctions.js
@@ -35,45 +35,43 @@ export function isInt(value) {
   }
 }
 
-function calculateTheSum(sum, symbol, currentDigit) {
-  const sumNumber = Number(sum);
-  const currentDigitNumber = Number(currentDigit);
+function calculateCurrentSum(symbol, currentValue) {
+  const sumNumber = Number(valuesObject.sum);
+  const currentDigitNumber = Number(currentValue);
   if (THE_SUM_SYMBOLS[symbol]) {
-    return (sum = action[symbol](sumNumber, currentDigitNumber));
+    valuesObject.sum = action[symbol](sumNumber, currentDigitNumber);
   } else if (!symbol) {
-    return (sum += Number(currentDigit));
+    valuesObject.sum = Number(currentValue);
   }
 }
 
-const calculateSumWithPercentAndSquare = (value, index, sum) => {
+const calculateSumWithPercentAndSquare = (value, index) => {
   value = value.toString();
   value = value.slice(0, value.length - 1);
 
   if (index > 0) {
     const square = action['√'](value);
     const percent = action['1%'](Number(square));
-    sum += percent;
+    valuesObject.sum += percent;
   } else {
-    sum += Number(action['√'](value));
-    sum = action['1%'](Number(sum));
+    valuesObject.sum = Number(action['√'](value));
+    valuesObject.sum = action['1%'](Number(valuesObject.sum));
   }
   value = value.split('');
   value.push('%');
   value = value.join('');
-
-  return sum;
 };
 
-const calculateSumWithPercentOrSquare = (values, index, sum) => {
+const calculateSumWithPercentOrSquare = (values, index) => {
   const hasValueIncludesPercent = checkForSymbol(values[index], [signs['%']]);
   const hasValueIncludesSquare = checkForSymbol(values[index], [signs['√']]);
 
   if (hasValueIncludesPercent) {
     if (index > 0) {
       values[index] = values[index].slice(0, values[index].length - 1);
-      sum = calculateTheSum(
+      calculateCurrentSum(
         values[index - 1],
-        action['2%'](Number(sum), Number(values[index]))
+        action['2%'](Number(valuesObject.sum), Number(values[index]))
       );
       values[index] = values[index].split('');
       values[index].push('%');
@@ -81,37 +79,29 @@ const calculateSumWithPercentOrSquare = (values, index, sum) => {
       return;
     }
     values[index] = values[index].slice(0, values[index].length - 1);
-    sum += action['1%'](Number(values[index]));
+    valuesObject.sum = action['1%'](Number(values[index]));
     values[index] = values[index].split('');
     values[index].push('%');
     values[index] = values[index].join('');
   } else if (hasValueIncludesSquare) {
-    sum = calculateTheSum(
-      sum,
-      values[index - 1],
-      Number(action['√'](values[index]))
-    );
+    calculateCurrentSum(values[index - 1], Number(action['√'](values[index])));
   }
-
-  return sum;
 };
 
 export function calculateSum(array) {
-  let sum = 0;
   for (let i = 0; i <= array.length - 1; i += 2) {
     const value = array[i].toString();
     const hasValueIncludesPercent = checkForSymbol(value, [signs['%']]);
     const hasValueIncludesSquare = checkForSymbol(value, [signs['√']]);
+
     if (hasValueIncludesPercent && hasValueIncludesSquare) {
-      sum = calculateSumWithPercentAndSquare(array[i], i, sum);
+      calculateSumWithPercentAndSquare(array[i], i);
     } else if (hasValueIncludesPercent || hasValueIncludesSquare) {
-      sum = calculateSumWithPercentOrSquare(array, i, sum);
+      calculateSumWithPercentOrSquare(array, i);
     } else {
-      sum = calculateTheSum(sum, array[i - 1], array[i]);
+      calculateCurrentSum(array[i - 1], array[i]);
     }
   }
-
-  return sum;
 }
 
 export const printResult = () =>
@@ -124,11 +114,25 @@ export const checkForSymbol = (data, symbols) => {
   if (Array.isArray(symbols) && symbols?.length > 0 && isDataString) {
     for (const symbol of symbols) {
       const hasSymbolIncluded = stringData?.includes(symbol);
-      if (!hasSymbolIncluded) return false;
+      if (hasSymbolIncluded) return true;
     }
-    return true;
+    return false;
   }
 };
 
-export const checkIsNumber = (value) =>
-  typeof Number(value) === 'number' && !isNaN(Number(value));
+// to create better regex for select and extract number
+const numberRegex = /^\d*(\.\d{1,})?$/g;
+export const checkIsNumber = (value) => {
+  const stringValue = value.toString();
+  const checkValue = numberRegex.test(stringValue);
+  if (checkValue) {
+    const match = stringValue.match(numberRegex);
+    const matchConcatenation = match.join('');
+    return (
+      typeof Number(matchConcatenation) === 'number' &&
+      !isNaN(Number(matchConcatenation))
+    );
+  }
+
+  return false;
+};
