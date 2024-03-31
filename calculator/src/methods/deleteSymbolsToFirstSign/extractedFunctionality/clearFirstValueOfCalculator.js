ce182440f0f7add98b62403ab6f34ecb1b3be076
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-03-31 15:12:52 +0300

    do some changes in clearFirstValueOfCalculator function to be more secured and safety

--- calculator/src/methods/deleteSymbolsToFirstSign/extractedFunctionality/clearFirstValueOfCalculator.js
+++ calculator/src/methods/deleteSymbolsToFirstSign/extractedFunctionality/clearFirstValueOfCalculator.js
@@ -6,11 +6,12 @@ import {
 
 export const clearFirstValueOfCalculator = () => {
   const index = textAreaValue.length - 1;
-  const value = textAreaValue[index];
+  const value = textAreaValue[index]?.toString();
   const hasValueLength = !!value?.length;
 
   const isValueLengthOne = hasValueLength && value.length === 1;
   const isValueLengthTwo = hasValueLength && value.length === 2;
+  const isValueNegative = textAreaValue[index] < 0;
 
   if (!hasValueLength) {
     return;
@@ -21,14 +22,16 @@ export const clearFirstValueOfCalculator = () => {
     signs['±'],
   ]);
 
-  const hasDeleteAllValue = isValueLengthTwo && isValueSquareOrMinusPlus;
+  const hasDeleteAllValue =
+    (isValueLengthTwo && isValueSquareOrMinusPlus) ||
+    (isValueLengthTwo && isValueNegative);
   const hasClearAllIndex = !!isValueLengthOne;
   if (hasClearAllIndex) {
     textAreaValue.pop();
   } else if (hasDeleteAllValue) {
     textAreaValue.splice(index, 1);
   } else if (hasValueLength) {
-    valuesObject.firstDigit = textAreaValue[index].slice(0, -1);
+    valuesObject.firstDigit = value.slice(0, -1);
     textAreaValue[index] = valuesObject.firstDigit;
     return;
   }
