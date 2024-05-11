5c965eb9c5a03716989791220862d34ab7d9eeb7
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-05-11 09:11:05 +0300

    create a config file for API request information

---
+++ weather-app/src/hooks/config.ts
@@ -0,0 +1,6 @@
+export const apiKey = 'z3nfDV7VK95r24v6ryzPCLTeAwTk1FAW';
+export const acceptQueryParam = '&accept=application/json';
+export const weatherUrls = {
+  forecastProgramUrl: 'https://api.tomorrow.io/v4/weather/forecast',
+  forecastDailyUrl: 'https://api.tomorrow.io/v4/weather/history/recent',
+};
