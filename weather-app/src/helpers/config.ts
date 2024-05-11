70918c726cbc9e1769ba8c0adebe559db9e0974f
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-05-11 09:09:45 +0300

    create a config file to save some of predefined values

---
+++ weather-app/src/helpers/config.ts
@@ -0,0 +1,14 @@
+export const TEMPERATURE_TYPE = {
+  celsius: '°C',
+  fahrenheit: '°F',
+};
+
+export const TEMPERATURE_SIGNS = {
+  plus: '+',
+  minus: '-',
+};
+
+export const TEMPERATURE_COLORS = {
+  plus: 'rgb(255, 220, 20)',
+  minus: 'rgb(20, 161, 255)',
+};
