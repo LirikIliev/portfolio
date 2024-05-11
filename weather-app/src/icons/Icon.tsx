76e6bccf1239ea67389ba88bb024d239f25cad47
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-05-11 09:11:49 +0300

    create a Icon wrapper component

---
+++ weather-app/src/icons/Icon.tsx
@@ -0,0 +1,86 @@
+import {
+  ClearDay,
+  Cloudy,
+  Drizzle,
+  Flurries,
+  Fog,
+  FreezingDrizzle,
+  FreezingRain,
+  HeavyRain,
+  HeavySnow,
+  IcePellets,
+  LightRain,
+  LightSnow,
+  Location,
+  PartlyCloudyDay,
+  Rain,
+  Search,
+  Snow,
+  Thermometer,
+  Thunderstorm,
+  Unknown,
+} from './';
+
+import classes from './Icon.module.scss';
+
+interface IconWrapperInterface {
+  iconName: string;
+  size: number;
+  metrics?: string;
+  styles?: {};
+}
+interface IconMapInterface {
+  [key: string]: JSX.Element;
+}
+
+const IconMap: IconMapInterface = {
+  Thermometer: <Thermometer />,
+  Search: <Search />,
+  Unknown: <Unknown />,
+  'Clear, Sunny': <ClearDay />,
+  'Mostly Clear': <PartlyCloudyDay />,
+  'Partly Cloudy': <PartlyCloudyDay />,
+  'Mostly Cloudy': <PartlyCloudyDay />,
+  Cloudy: <Cloudy />,
+  Fog: <Fog />,
+  'Light Fog': <Fog />,
+  Drizzle: <Drizzle />,
+  Rain: <Rain />,
+  'Light Rain': <LightRain />,
+  'Heavy Rain': <HeavyRain />,
+  Snow: <Snow />,
+  Flurries: <Flurries />,
+  'Light Snow': <LightSnow />,
+  'Heavy Snow': <HeavySnow />,
+  'Freezing Drizzle': <FreezingDrizzle />,
+  'Freezing Rain': <FreezingRain />,
+  'Light Freezing Rain': <FreezingRain />,
+  'Heavy Freezing Rain': <FreezingRain />,
+  'Ice Pellets': <IcePellets />,
+  'Heavy Ice Pellets': <IcePellets />,
+  'Light Ice Pellets': <IcePellets />,
+  Thunderstorm: <Thunderstorm />,
+  Location: <Location />,
+};
+
+const Icon: React.FC<IconWrapperInterface> = ({
+  iconName,
+  size,
+  metrics,
+  styles,
+}) => {
+  return (
+    <div
+      style={{
+        width: `${size}${metrics ? metrics : '%'}`,
+        height: `${size}${metrics ? metrics : '%'}`,
+        ...styles,
+      }}
+      className={classes['Wrapper']}
+    >
+      {IconMap[iconName]}
+    </div>
+  );
+};
+
+export default Icon;
