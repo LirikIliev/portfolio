359a2b7fd69f75706714ce49ab6e74bc25264dd3
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-05-27 13:04:40 +0300

    add logic for responsive design

--- weather-app/src/components/Header/Header.tsx
+++ weather-app/src/components/Header/Header.tsx
@@ -5,44 +5,51 @@ import Icon from '../../icons/Icon';
 import { TEMPERATURE_TYPE } from '../../helpers/config';
 
 import classes from './Header.module.scss';
+import { useMobileScreenDetection } from '../../hooks/useMobileScreenDetection';
 
 const Header: React.FC = () => {
   const {
-    locationPermission,
     locationName,
     onTemperatureSignChange,
     isSignCelsius,
     typeOfTemperature,
   } = useContext(ForecastContext);
+  const { isMobile } = useMobileScreenDetection();
 
   return (
-    <nav className={classes['Wrapper']}>
+    <div className={classes['Wrapper']}>
       <div>
         <p className={classes['Header']}>Synoptic</p>
       </div>
+      {locationName ? (
+        <button
+          className={classes['Temperature-sign-button']}
+          onClick={onTemperatureSignChange}
+        >
+          <strong className={classes['Selected-temperature-sign']}>
+            {typeOfTemperature}
+          </strong>{' '}
+          <p>|</p>
+          <p>
+            {isSignCelsius
+              ? TEMPERATURE_TYPE.fahrenheit
+              : TEMPERATURE_TYPE.celsius}
+          </p>
+        </button>
+      ) : null}
       <ul className={classes['Ul-list']}>
-        {locationPermission ? (
+        {locationName ? (
           <li className={classes['Location-wrapper']}>
-            <Icon iconName={'Location'} size={35} metrics="px" />
+            <Icon
+              iconName={'Location'}
+              size={isMobile ? 20 : 35}
+              metrics="px"
+            />
             <p className={classes['Text']}>{locationName}</p>
           </li>
         ) : null}
       </ul>
-      <button
-        className={classes['Temperature-sign-button']}
-        onClick={onTemperatureSignChange}
-      >
-        <strong className={classes['Selected-temperature-sign']}>
-          {typeOfTemperature}
-        </strong>{' '}
-        <p>|</p>
-        <p>
-          {isSignCelsius
-            ? TEMPERATURE_TYPE.fahrenheit
-            : TEMPERATURE_TYPE.celsius}
-        </p>
-      </button>
-    </nav>
+    </div>
   );
 };
 
