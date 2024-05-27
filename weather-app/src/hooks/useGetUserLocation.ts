7fb95b22143dd491b1219ed2c2e1af9eb6c88ff2
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-05-27 13:20:17 +0300

    update hook who mush check if user access geo location and on base of that to make a request with this location information

--- weather-app/src/hooks/useGetUserLocation.ts
+++ weather-app/src/hooks/useGetUserLocation.ts
@@ -1,26 +1,17 @@
-import { SetStateAction, useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 
-import { LatitudeLongitudeInterface } from '../types/hookTypes';
-import { useFetchForecastProgramAPI } from './useFetchForecastProgramAPI';
-import { useFetchForecastDailyAPI } from './useFetchForecastDailyHourlyAPI';
 import { ForecastContext } from '../context/ForecastContext';
 
 const useGetUserLocation = () => {
-  const [userLocation, setUserLocation] =
-    useState<LatitudeLongitudeInterface | null>();
-  const [geoLocationError, setGeoLocationError] =
-    useState<SetStateAction<string>>('');
-  const { getProgramForecast } = useFetchForecastProgramAPI();
-  const { getDailyForecast } = useFetchForecastDailyAPI();
-  const { setLocationPermission } = useContext(ForecastContext);
+  const { setLocationName, setUserLocationInfo, setGeoLocationError } =
+    useContext(ForecastContext);
 
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
-          setUserLocation({ latitude, longitude });
-          setLocationPermission(true);
+          setUserLocationInfo({ latitude, longitude });
         },
         () => {
           setGeoLocationError('Error getting user location');
@@ -29,18 +20,7 @@ const useGetUserLocation = () => {
     } else {
       setGeoLocationError('Geolocation is not supported by this browser.');
     }
-  }, [setLocationPermission]);
-
-  // useEffect(() => {
-  //   if (
-  //     !geoLocationError &&
-  //     userLocation instanceof Object &&
-  //     userLocation.latitude &&
-  //     userLocation.longitude
-  //   )
-  //   getProgramForecast(userLocation);
-  //   getDailyForecast(userLocation);
-  // }, [geoLocationError, getDailyForecast, getProgramForecast, userLocation]);
+  }, [setGeoLocationError, setLocationName, setUserLocationInfo]);
 };
 
 export default useGetUserLocation;
