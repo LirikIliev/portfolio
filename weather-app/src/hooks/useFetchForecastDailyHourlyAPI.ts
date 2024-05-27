3adaa7dbac7655cd96570921b174d028766b5fa3
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-05-27 13:19:11 +0300

    update response handling in useFetchForecastDailyHourlyAPI

--- weather-app/src/hooks/useFetchForecastDailyHourlyAPI.ts
+++ weather-app/src/hooks/useFetchForecastDailyHourlyAPI.ts
@@ -1,8 +1,11 @@
-import { useContext } from 'react';
+import { useCallback, useContext } from 'react';
 import { DailyInfoInterface, LocationInterface } from '../types/hookTypes';
 import { acceptQueryParam, apiKey, weatherUrls } from './config';
 import { ForecastContext } from '../context/ForecastContext';
-import { TEMPERATURE_COLORS } from '../helpers/config';
+import {
+  TEMPERATURE_COLORS,
+  defaultDailyForecastValue,
+} from '../helpers/config';
 import { convertCelsiusToFahrenheit } from '../helpers/convertCelsiusToFahrenheit';
 import { roundTemperatureValues } from '../helpers/roundTemperatureValues';
 
@@ -10,69 +13,80 @@ export const useFetchForecastDailyAPI = () => {
   const { setDailyForecast, setDailyError, setDailyLoading } =
     useContext(ForecastContext);
 
-  const getDailyForecast = async (location: LocationInterface) => {
-    try {
-      setDailyLoading(true);
-      const isFetchOnGeoLocation =
-        location instanceof Object &&
-        location.hasOwnProperty('Latitude') &&
-        location.hasOwnProperty('Longitude');
-      const forecastRequest = await fetch(
-        `${weatherUrls.forecastDailyUrl}?location=${
-          isFetchOnGeoLocation
-            ? location.latitude + ',' + location.longitude
-            : location
-        }&apikey=${apiKey}${acceptQueryParam}`
-      );
+  const getDailyForecast = useCallback(
+    async (location: LocationInterface) => {
+      try {
+        setDailyLoading(true);
+        const isFetchOnGeoLocation =
+          location instanceof Object &&
+          location.hasOwnProperty('Latitude') &&
+          location.hasOwnProperty('Longitude');
+        const forecastResponse = await fetch(
+          `${weatherUrls.forecastDailyUrl}?location=${
+            isFetchOnGeoLocation
+              ? location.latitude + ',' + location.longitude
+              : location
+          }&apikey=${apiKey}${acceptQueryParam}`
+        );
 
-      if (forecastRequest.status === 200) {
-        const parsedResponse = await forecastRequest.json();
-        setDailyForecast(parsedResponse);
-
-        if (
-          parsedResponse?.timelines.daily?.[0]?.values &&
-          Array.isArray(parsedResponse?.timelines.daily)
-        ) {
-          parsedResponse?.timelines?.hourly?.forEach(
-            (hourlyData: DailyInfoInterface) => {
-              console.log(hourlyData);
-              if (hourlyData.values?.temperature) {
-                hourlyData.values.temperature = roundTemperatureValues(
-                  hourlyData.values.temperature
-                );
-
-                if (typeof hourlyData.values.temperature === 'number')
-                  hourlyData.values.temperature > 0
-                    ? (hourlyData.values.temperatureSign = '+')
-                    : (hourlyData.values.temperatureSign = '-');
-
-                const temperature = hourlyData.values.temperature;
-                hourlyData.values.temperatureColor =
-                  typeof temperature === 'number' && temperature > 0
-                    ? TEMPERATURE_COLORS.plus
-                    : TEMPERATURE_COLORS.minus;
-                hourlyData.values.temperatureFahrenheit =
-                  convertCelsiusToFahrenheit(
-                    typeof temperature === 'number' ? temperature : null
-                  );
-              }
-            }
-          );
+        if (!forecastResponse.ok) {
+          const errorResponseHandling = await forecastResponse.json();
+          throw errorResponseHandling;
         }
-      }
-    } catch (error: any) {
-      if (
-        error instanceof Object &&
-        error?.hasOwnProperty('code') &&
-        error.hasOwnProperty('type') &&
-        error.hasOwnProperty('message')
-      ) {
+
+        if (forecastResponse.status === 200) {
+          const parsedResponse = await forecastResponse.json();
+          setDailyForecast(parsedResponse);
+
+          if (
+            parsedResponse?.timelines.daily?.[0]?.values &&
+            Array.isArray(parsedResponse?.timelines.daily)
+          ) {
+            parsedResponse?.timelines?.hourly?.forEach(
+              (hourlyData: DailyInfoInterface) => {
+                if (hourlyData.values?.temperature) {
+                  hourlyData.values.temperature = roundTemperatureValues(
+                    hourlyData.values.temperature
+                  );
+
+                  if (typeof hourlyData.values.temperature === 'number')
+                    hourlyData.values.temperature > 0
+                      ? (hourlyData.values.temperatureSign = '+')
+                      : (hourlyData.values.temperatureSign = '-');
+
+                  const temperature = hourlyData.values.temperature;
+                  hourlyData.values.temperatureColor =
+                    typeof temperature === 'number' && temperature > 0
+                      ? TEMPERATURE_COLORS.plus
+                      : TEMPERATURE_COLORS.minus;
+                  hourlyData.values.temperatureFahrenheit =
+                    convertCelsiusToFahrenheit(
+                      typeof temperature === 'number' ? temperature : null
+                    );
+                }
+              }
+            );
+          }
+        }
+      } catch (error: any) {
+        if (
+          error instanceof Object &&
+          error?.hasOwnProperty('code') &&
+          error.hasOwnProperty('type') &&
+          error.hasOwnProperty('message')
+        ) {
+          setDailyError(error);
+        } else {
+          setDailyError(error);
+        }
         setDailyError(error);
+        setDailyForecast(defaultDailyForecastValue);
+      } finally {
+        setDailyLoading(false);
       }
-    } finally {
-      setDailyLoading(false);
-    }
-  };
+    },
+    [setDailyError, setDailyForecast, setDailyLoading]
+  );
 
   return {
     getDailyForecast,
