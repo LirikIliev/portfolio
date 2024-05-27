381d2e8c6b2b5341f7336ceb0036cb7e4da203c8
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-05-27 10:23:01 +0300

    make a change to set a logic for make a request with user location data on initial loading of site

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,38 +1,69 @@
-import { useContext } from 'react';
+import { memo, useContext, useEffect } from 'react';
 
-import useGetUserLocation from './hooks/useGetUserLocation';
 import { ForecastContext } from './context/ForecastContext';
 import LocationSearch from './components/LocationSearch/LocationSearch';
 import Header from './components/Header/Header';
 import DailyForecastTemplateWrapper from './components/ProgramForecastTemplate/ForecastProgramTemplateWrapper';
+import CurrentForecastWrapper from './components/CurrentForecastTemplate/CurrentForecastWrapper';
+import Modal from './components/Modal/Modal';
 
 import './App.css';
-import CurrentForecastWrapper from './components/CurrentForecastTemplate/CurrentForecastWrapper';
+import Errors from './components/Errors/Errors';
+import { useFetchForecastProgramAPI } from './hooks/useFetchForecastProgramAPI';
+import { useFetchForecastDailyAPI } from './hooks/useFetchForecastDailyHourlyAPI';
+import useGetUserLocation from './hooks/useGetUserLocation';
 
 const App = () => {
-  useGetUserLocation();
   const {
+    isModalOpen,
     programForecast: {
       timelines: { daily, hourly },
     },
+    geoLocationError,
+    userLocationInfo,
   } = useContext(ForecastContext);
+  const { getProgramForecast } = useFetchForecastProgramAPI();
+  const { getDailyForecast } = useFetchForecastDailyAPI();
+  useGetUserLocation();
+
+  useEffect(() => {
+    if (
+      !geoLocationError &&
+      userLocationInfo instanceof Object &&
+      userLocationInfo.latitude &&
+      userLocationInfo.longitude
+    ) {
+      getProgramForecast(userLocationInfo);
+      getDailyForecast(userLocationInfo);
+    }
+  }, [
+    geoLocationError,
+    getDailyForecast,
+    getProgramForecast,
+    userLocationInfo,
+  ]);
 
   const isForecastAvailable = daily.length > 0 && hourly.length > 0;
 
   return (
     <div className="App">
-      <section className="Header-section">
+      <header className="header-section">
         <Header />
         <LocationSearch />
-      </section>
-      {isForecastAvailable ? (
-        <section className="Body-section">
-          <CurrentForecastWrapper />
-          <DailyForecastTemplateWrapper />
-        </section>
-      ) : null}
+      </header>
+      <main className="main-section">
+        {isForecastAvailable ? (
+          <>
+            <CurrentForecastWrapper />
+            <DailyForecastTemplateWrapper />
+          </>
+        ) : (
+          <Errors />
+        )}
+      </main>
+      {isModalOpen ? <Modal /> : null}
     </div>
   );
 };
 
-export default App;
+export default memo(App);
