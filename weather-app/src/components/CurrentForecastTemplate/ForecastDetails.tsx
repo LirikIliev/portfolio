e9253cd35ffb6640dde081f1d77de71b38d9e207
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-05-27 10:26:43 +0300

    create a forecast detail component

---
+++ weather-app/src/components/CurrentForecastTemplate/ForecastDetails.tsx
@@ -0,0 +1,67 @@
+import { useContext } from 'react';
+
+import { ForecastContext } from '../../context/ForecastContext';
+
+import classes from './ForecastDetails.module.scss';
+
+const ForecastDetails: React.FC = () => {
+  const {
+    programForecast: {
+      timelines: { daily },
+    },
+  } = useContext(ForecastContext);
+
+  const {
+    values: {
+      windSpeedMin = '',
+      windSpeedMax = '',
+      humidityMin = '',
+      humidityMax = '',
+      pressureSurfaceLevelMin = '',
+      pressureSurfaceLevelMax = '',
+      precipitationProbabilityAvg = '',
+    } = {},
+  } = daily?.[0];
+
+  return (
+    <div className={classes['Wrapper']}>
+      <div>
+        <p className={classes['Title']}>More Details:</p>
+      </div>
+      {windSpeedMin && windSpeedMax ? (
+        <div className={classes['WindSpeed-wrapper']}>
+          <p className={classes['Info']}>
+            <strong className={classes['Info-title']}>Wind speed: </strong>
+            {windSpeedMin}/{windSpeedMax}ms.
+          </p>
+        </div>
+      ) : null}
+      {humidityMin && humidityMax ? (
+        <div className={classes['Humidity-wrapper']}>
+          <p className={classes['Info']}>
+            <strong className={classes['Info-title']}>Air humidity: </strong>
+            {windSpeedMin}/{windSpeedMax}%
+          </p>
+        </div>
+      ) : null}
+      {pressureSurfaceLevelMin && pressureSurfaceLevelMax ? (
+        <div className={classes['PressureSurf-wrapper']}>
+          <p className={classes['Info']}>
+            <strong className={classes['Info-title']}>Pressure: </strong>
+            {pressureSurfaceLevelMin}/{pressureSurfaceLevelMax}mm.
+          </p>
+        </div>
+      ) : null}
+      {pressureSurfaceLevelMin && pressureSurfaceLevelMax ? (
+        <div className={classes['Precipitation-wrapper']}>
+          <p className={classes['Info']}>
+            <strong className={classes['Info-title']}>Precipitation: </strong>
+            {precipitationProbabilityAvg}%
+          </p>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default ForecastDetails;
