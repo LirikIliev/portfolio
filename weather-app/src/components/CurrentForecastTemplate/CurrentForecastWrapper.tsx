c48ebb369650126bccc59ae6dad85c03d353e54b
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-05-27 10:24:53 +0300

    create current forecast weather wrapper component

--- weather-app/src/components/CurrentForecastTemplate/CurrentForecastWrapper.tsx
+++ weather-app/src/components/CurrentForecastTemplate/CurrentForecastWrapper.tsx
@@ -1,15 +1,46 @@
 import CurrentTemperature from './CurrentTemperature';
 
-import classes from './CurrentForecastWrapper.module.scss';
-import MoreForecastDetails from './MoreForecastDetails';
+import ForecastDetails from './ForecastDetails';
 import HourlyForecastWrapper from './HourlyForecastWrapper';
+import { useContext } from 'react';
+import { ForecastContext } from '../../context/ForecastContext';
+import LoadingScreen from '../LoadingScreen/LoadingScreen';
 
-const CurrentForecastWrapper: React.FC = () => (
-  <div className={classes['Wrapper']}>
-    <CurrentTemperature />
-    <MoreForecastDetails />
-    <HourlyForecastWrapper />
-  </div>
-);
+import classes from './CurrentForecastWrapper.module.scss';
+
+const CurrentForecastWrapper: React.FC = () => {
+  const { dailyForecast, dailyLoading } = useContext(ForecastContext);
+
+  const {
+    timelines: { daily, hourly },
+  } = dailyForecast;
+
+  const isHourlyInformationAvailable =
+    !dailyLoading && hourly && hourly?.length > 0;
+  const isDailyInformationAvailable =
+    !dailyLoading && daily && daily.length > 0;
+
+  return (
+    <div
+      className={
+        classes[
+          isDailyInformationAvailable && isHourlyInformationAvailable
+            ? 'Wrapper'
+            : 'Loading_wrapper'
+        ]
+      }
+    >
+      {isDailyInformationAvailable && isHourlyInformationAvailable ? (
+        <>
+          <CurrentTemperature />
+          <ForecastDetails />
+          <HourlyForecastWrapper />
+        </>
+      ) : (
+        <LoadingScreen />
+      )}
+    </div>
+  );
+};
 
 export default CurrentForecastWrapper;
